1d9c76033ccba7f99f747ce88a3e1f9f92dd032c
Handicap <sambzalgod_32@hotmail.com>
2015-09-28 20:41:55 +0300

    Raffling works! Still have to implement menu and graphics

--- Assets/Assets/Script/RaffleController.cs
+++ Assets/Assets/Script/RaffleController.cs
@@ -20,7 +20,11 @@ public class RaffleController : MonoBehaviour {
     public Text seattxt;
 
     public float phase = 0f;
+    public float localphase = 0f;
+    public float localmax;
 
+    public int winningrow;
+    public int winningseat;
 	// Use this for initialization
 	void Start () {
         
@@ -35,7 +39,10 @@ public class RaffleController : MonoBehaviour {
     
     }
 
-
+    /// <summary>
+    /// Use this method to start the raffling process.
+    /// The method checks which part of the raffle is going on and automatically figures out the rest.
+    /// </summary>
     public void StartRaffle()
     {
         spinbtn.interactable = false;
@@ -47,29 +54,73 @@ public class RaffleController : MonoBehaviour {
         else
         {
             spinneri.StartSpinner(spinningtime);
+            StartCoroutine(RaffleSeats());
         }
     }
 
+    /// <summary>
+    /// Randomize the row (use the list to get the different rows)
+    /// </summary>
+    /// <returns>This is a coroutine</returns>
     private IEnumerator RaffleRows()
     {
-
+        //float localphase = 0f;
+        localmax = 1f / (containerlist.Count*30);
         Debug.Log("entered coroutine - rafflerows");
         //float phase = 0f;
+        Debug.Log("localmax " + localmax);
         while (phase < 1f)
         {
-            rowtxt.text = containerlist[Random.Range(0, containerlist.Count)].rowname;
+            if (localphase > localmax)
+            {
+                winningrow = Random.Range(0, containerlist.Count);
+                rowtxt.text = containerlist[winningrow].rowname;
+                localphase = 0f;
+            }
+
+            localmax = localmax + ((localmax * Time.deltaTime)/2);
             phase = phase + (Time.deltaTime / spinningtime);
+            localphase = localphase + (Time.deltaTime / spinningtime);
             yield return new WaitForEndOfFrame();
         }
         Debug.Log("exiting coroutine - rafflerows");
         phase = 0f;
+        localphase = 0f;
         spinbtn.interactable = true;
+        rowsdone = true;
 
         yield return new WaitForEndOfFrame();
     }
 
+    /// <summary>
+    /// Randomize the seats. The row is determined by the RaffleRows method.
+    /// </summary>
+    /// <returns>Coroutine</returns>
     private IEnumerator RaffleSeats()
     {
+        //float localphase = 0f;
+        localmax = 1f / ((containerlist[winningrow].rowseats + 1) * 3);
+        Debug.Log("entered coroutine - raffleseats");
+        //float phase = 0f;
+        Debug.Log("localmax " + localmax);
+        while (phase < 1f)
+        {
+            if (localphase > localmax)
+            {
+                winningseat = Random.Range(1, containerlist[winningrow].rowseats + 1);
+                seattxt.text = winningseat.ToString();
+                localphase = 0f;
+            }
+
+            localmax = localmax + ((localmax * Time.deltaTime) / 2);
+            phase = phase + (Time.deltaTime / spinningtime);
+            localphase = localphase + (Time.deltaTime / spinningtime);
+            yield return new WaitForEndOfFrame();
+        }
+        Debug.Log("exiting coroutine - raffleseats");
+        phase = 0f;
+        localphase = 0f;
+        spinbtn.interactable = true;
 
         yield return new WaitForEndOfFrame();
     }
